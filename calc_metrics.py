2387656f63905a2da742dec17034b6137c59e223
duskvirkus <duskvirkus@protonmail.com>
2021-06-20 10:34:45 -0600

    tpu first commit

--- calc_metrics.py
+++ calc_metrics.py
@@ -17,89 +17,95 @@ import dnnlib.tflib as tflib
 
 from metrics import metric_defaults
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 class UserError(Exception):
-    pass
+  pass
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def calc_metrics(network_pkl, metric_names, metricdata, mirror, gpus):
-    tflib.init_tf()
+  tflib.init_tf()
 
-    # Initialize metrics.
-    metrics = []
-    for name in metric_names:
-        if name not in metric_defaults.metric_defaults:
-            raise UserError('\n'.join(['--metrics can only contain the following values:', 'none'] + list(metric_defaults.metric_defaults.keys())))
-        metrics.append(dnnlib.util.construct_class_by_name(**metric_defaults.metric_defaults[name]))
+  # Initialize metrics.
+  metrics = []
+  for name in metric_names:
+    if name not in metric_defaults.metric_defaults:
+      raise UserError('\n'.join(
+        ['--metrics can only contain the following values:', 'none'] + list(metric_defaults.metric_defaults.keys())))
+    metrics.append(dnnlib.util.construct_class_by_name(**metric_defaults.metric_defaults[name]))
 
-    # Load network.
-    if not dnnlib.util.is_url(network_pkl, allow_file_urls=True) and not os.path.isfile(network_pkl):
-        raise UserError('--network must point to a file or URL')
-    print(f'Loading network from "{network_pkl}"...')
-    with dnnlib.util.open_url(network_pkl) as f:
-        _G, _D, Gs = pickle.load(f)
-        Gs.print_layers()
+  # Load network.
+  if not dnnlib.util.is_url(network_pkl, allow_file_urls=True) and not os.path.isfile(network_pkl):
+    raise UserError('--network must point to a file or URL')
+  print(f'Loading network from "{network_pkl}"...')
+  with dnnlib.util.open_url(network_pkl) as f:
+    _G, _D, Gs = pickle.load(f)
+    Gs.print_layers()
 
-    # Look up training options.
-    run_dir = None
-    training_options = None
-    if os.path.isfile(network_pkl):
-        potential_run_dir = os.path.dirname(network_pkl)
-        potential_json_file = os.path.join(potential_run_dir, 'training_options.json')
-        if os.path.isfile(potential_json_file):
-            print(f'Looking up training options from "{potential_json_file}"...')
-            run_dir = potential_run_dir
-            with open(potential_json_file, 'rt') as f:
-                training_options = json.load(f, object_pairs_hook=dnnlib.EasyDict)
-    if training_options is None:
-        print('Could not look up training options; will rely on --metricdata and --mirror')
+  # Look up training options.
+  run_dir = None
+  training_options = None
+  if os.path.isfile(network_pkl):
+    potential_run_dir = os.path.dirname(network_pkl)
+    potential_json_file = os.path.join(potential_run_dir, 'training_options.json')
+    if os.path.isfile(potential_json_file):
+      print(f'Looking up training options from "{potential_json_file}"...')
+      run_dir = potential_run_dir
+      with open(potential_json_file, 'rt') as f:
+        training_options = json.load(f, object_pairs_hook=dnnlib.EasyDict)
+  if training_options is None:
+    print('Could not look up training options; will rely on --metricdata and --mirror')
 
-    # Choose dataset options.
-    dataset_options = dnnlib.EasyDict()
-    if training_options is not None:
-        dataset_options.update(training_options.metric_dataset_args)
-    dataset_options.resolution = Gs.output_shapes[0][-1]
-    dataset_options.max_label_size = Gs.input_shapes[1][-1]
-    if metricdata is not None:
-        if not os.path.isdir(metricdata):
-            raise UserError('--metricdata must point to a directory containing *.tfrecords')
-        dataset_options.path = metricdata
-    if mirror is not None:
-        dataset_options.mirror_augment = mirror
-    if 'path' not in dataset_options:
-        raise UserError('--metricdata must be specified explicitly')
+  # Choose dataset options.
+  dataset_options = dnnlib.EasyDict()
+  if training_options is not None:
+    dataset_options.update(training_options.metric_dataset_args)
+  dataset_options.resolution = Gs.output_shapes[0][-1]
+  dataset_options.max_label_size = Gs.input_shapes[1][-1]
+  if metricdata is not None:
+    if not os.path.isdir(metricdata):
+      raise UserError('--metricdata must point to a directory containing *.tfrecords')
+    dataset_options.path = metricdata
+  if mirror is not None:
+    dataset_options.mirror_augment = mirror
+  if 'path' not in dataset_options:
+    raise UserError('--metricdata must be specified explicitly')
 
-    # Print dataset options.
+  # Print dataset options.
+  print()
+  print('Dataset options:')
+  print(json.dumps(dataset_options, indent=2))
+
+  # Evaluate metrics.
+  for metric in metrics:
     print()
-    print('Dataset options:')
-    print(json.dumps(dataset_options, indent=2))
+    print(f'Evaluating {metric.name}...')
+    metric.configure(dataset_args=dataset_options, run_dir=run_dir)
+    metric.run(network_pkl=network_pkl, num_gpus=gpus)
 
-    # Evaluate metrics.
-    for metric in metrics:
-        print()
-        print(f'Evaluating {metric.name}...')
-        metric.configure(dataset_args=dataset_options, run_dir=run_dir)
-        metric.run(network_pkl=network_pkl, num_gpus=gpus)
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def _str_to_bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    if v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    raise argparse.ArgumentTypeError('Boolean value expected.')
+  if isinstance(v, bool):
+    return v
+  if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    return True
+  if v.lower() in ('no', 'false', 'f', 'n', '0'):
+    return False
+  raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def _parse_comma_sep(s):
-    if s is None or s.lower() == 'none' or s == '':
-        return []
-    return s.split(',')
+  if s is None or s.lower() == 'none' or s == '':
+    return []
+  return s.split(',')
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 _cmdline_help_epilog = '''examples:
 
@@ -133,31 +139,38 @@ available metrics:
     ls           Linear separability with respect to CelebA attributes.
 '''
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Calculate quality metrics for previous training run or pretrained network pickle.',
-        epilog=_cmdline_help_epilog,
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
+  parser = argparse.ArgumentParser(
+    description='Calculate quality metrics for previous training run or pretrained network pickle.',
+    epilog=_cmdline_help_epilog,
+    formatter_class=argparse.RawDescriptionHelpFormatter
+  )
 
-    parser.add_argument('--network',    help='Network pickle filename or URL', dest='network_pkl', metavar='PATH')
-    parser.add_argument('--metrics',    help='Comma-separated list or "none" (default: %(default)s)', dest='metric_names', type=_parse_comma_sep, default='fid50k_full', metavar='LIST')
-    parser.add_argument('--metricdata', help='Dataset to evaluate metrics against (default: look up from training options)', metavar='PATH')
-    parser.add_argument('--mirror',     help='Whether the dataset was augmented with x-flips during training (default: look up from training options)', type=_str_to_bool, metavar='BOOL')
-    parser.add_argument('--gpus',       help='Number of GPUs to use (default: %(default)s)', type=int, default=1, metavar='INT')
+  parser.add_argument('--network', help='Network pickle filename or URL', dest='network_pkl', metavar='PATH')
+  parser.add_argument('--metrics', help='Comma-separated list or "none" (default: %(default)s)', dest='metric_names',
+                      type=_parse_comma_sep, default='fid50k_full', metavar='LIST')
+  parser.add_argument('--metricdata',
+                      help='Dataset to evaluate metrics against (default: look up from training options)',
+                      metavar='PATH')
+  parser.add_argument('--mirror',
+                      help='Whether the dataset was augmented with x-flips during training (default: look up from training options)',
+                      type=_str_to_bool, metavar='BOOL')
+  parser.add_argument('--gpus', help='Number of GPUs to use (default: %(default)s)', type=int, default=1, metavar='INT')
 
-    args = parser.parse_args()
-    try:
-        calc_metrics(**vars(args))
-    except UserError as err:
-        print(f'Error: {err}')
-        exit(1)
+  args = parser.parse_args()
+  try:
+    calc_metrics(**vars(args))
+  except UserError as err:
+    print(f'Error: {err}')
+    exit(1)
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    main()
+  main()
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
